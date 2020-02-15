c6b82e24f65da4397b1d9d93a10546611cf66db3
Eduardo de Rivero Manrique <ederiveroman@gmail.com>
2020-02-15 14:16:07 -0500

    ejercicios

--- BackEnd/FullDay 15 de Febrero/09-ejercicios.py
+++ BackEnd/FullDay 15 de Febrero/09-ejercicios.py
@@ -2,3 +2,19 @@
 # primo => si solamente es divisible entre si y entre 1
 # 1 3 5 7 11 13 17 19 23 ....
 # https://matematicasies.com/Averiguar-si-un-numero-es-primo
+
+# Graficar un triangulo recto en consola dependiendo del numero de niveles ingresado por teclado
+# ingreso: 3
+# *
+# **
+# ***
+
+# ingreso: 6
+# *
+# **
+# ***
+# ****
+# *****
+# ******
+
+# Hacer una calculadora que me de un menu de opciones las cuales sean: sumar, restar, multiplicar y que me indique el resultado y si quiero seguir haciendo operaciones o escribir "salir" para salir del bucle

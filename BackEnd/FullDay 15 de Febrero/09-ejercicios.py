0fd48f218ab80d9f796b5a31bff54203c5cbe02b
Eduardo de Rivero Manrique <ederiveroman@gmail.com>
2020-02-15 14:16:29 -0500

    Update 09-ejercicios.py

--- BackEnd/FullDay 15 de Febrero/09-ejercicios.py
+++ BackEnd/FullDay 15 de Febrero/09-ejercicios.py
@@ -1,9 +1,9 @@
-# Teniendo una lista vacia, ingresar 5 numeros por el teclado e indicar cuales de ellos son primos
+# Ejercicio 1: Teniendo una lista vacia, ingresar 5 numeros por el teclado e indicar cuales de ellos son primos
 # primo => si solamente es divisible entre si y entre 1
 # 1 3 5 7 11 13 17 19 23 ....
 # https://matematicasies.com/Averiguar-si-un-numero-es-primo
 
-# Graficar un triangulo recto en consola dependiendo del numero de niveles ingresado por teclado
+# Ejercicio 2: Graficar un triangulo recto en consola dependiendo del numero de niveles ingresado por teclado
 # ingreso: 3
 # *
 # **
@@ -17,4 +17,4 @@
 # *****
 # ******
 
-# Hacer una calculadora que me de un menu de opciones las cuales sean: sumar, restar, multiplicar y que me indique el resultado y si quiero seguir haciendo operaciones o escribir "salir" para salir del bucle
+# Ejercicio 3: Hacer una calculadora que me de un menu de opciones las cuales sean: sumar, restar, multiplicar y que me indique el resultado y si quiero seguir haciendo operaciones o escribir "salir" para salir del bucle

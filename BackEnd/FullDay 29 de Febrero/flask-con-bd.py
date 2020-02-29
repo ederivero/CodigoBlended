fb35f2a35256652854a081743b3bdabef6993733
Eduardo de Rivero Manrique <ederiveroman@gmail.com>
2020-02-29 17:34:35 -0500

    avance

--- BackEnd/FullDay 29 de Febrero/flask-con-bd.py
+++ BackEnd/FullDay 29 de Febrero/flask-con-bd.py
@@ -88,12 +88,79 @@ def devolver_categoria(palabra):
         return 'No existe esa categoria'
 
 # Generar rutas para:
-# Ruta para Ingresar una persona (7min)
-# Ruta para Mostrar la lista de productos de esa persona (use el JOIN) enviada por la url (nombre de la persona) (20min)
-# Ruta para Traer la lista de todas las personas mujeres (6min)
-# Ruta para Traer la lista de todos los productos de una categoria enviada por el url (JOIN) (6min)
-# Ruta para Ingresar una relacion de una persona con el producto y su cantidad (5min)
 
+# Ruta para Ingresar una persona (7min)
+@app.route('/persona', methods=['POST'])
+def agregar():
+    data = request.get_json()
+    conexion = conexionMYSQL.connection.cursor()
+    conexion.execute("INSERT INTO t_persona (per_nom, per_ape_pa, per_ape_ma, per_sexo) VALUES (%s,%s,%s,%s)",(data['nombre'],data['ape_paterno'],data['ape_materno'],data['sexo']))
+    # Si es una clausula INSERT, UPDATE o DELETE tenemos que hacer commit para que los cambios tengan efecto en la BD
+    conexionMYSQL.connection.commit()
+    # Si es una clausula de LECTURA DE DATOS, tengo que usar el fetchone o el fetchall
+    conexion.close()
+    return jsonify({
+        'mensaje':'Creado con exito',
+        'contenido':data
+    }), 201
+    # Validar si en mi data tengo nombre, ape_paterno, ape_materno, sexo y si no tengo que me retorne un mensaje que faltan campos
+
+# Ruta para Mostrar la lista de productos de esa persona (use el JOIN) enviada por la url (nombre de la persona) (20min)
+@app.route('/persona/productos/<int:id>', methods=['GET'])
+def productos_persona(id):
+    conexion = conexionMYSQL.connection.cursor()
+
+    sentencia = f"""SELECT PER_NOM, PER_APE_PA, PER_APE_MA, PERPRO_CANT, PROD_NOM
+                    FROM T_PERSONA
+                    INNER JOIN T_PER_PRO ON T_PERSONA.PER_ID=t_per_pro.per_id
+                    INNER JOIN T_PRODUCTO ON t_per_pro.prod_id= T_PRODUCTO.PROD_ID
+                    WHERE T_PERSONA.PER_ID={id};"""
+    conexion.execute(sentencia)
+    informacion = conexion.fetchall()
+    resultadofinal=[]
+    for resultado in informacion:
+        diccionario={
+            'nombre':resultado[0],
+            'ape_pat':resultado[1],
+            'ape_mat':resultado[2],
+            'producto':resultado[3],
+            'cantidad':resultado[4]
+        }
+        resultadofinal.append(diccionario)
+    print(informacion)
+    conexion.close()
+    # Hacer una validacion, si no hay productos para mostrar que retorne que no hay productos, caso contrario que retorne:
+    return jsonify({
+        'mensaje':'Los productos son',
+        'contenido':resultadofinal
+    })
+# Ruta para Traer la lista de todas las personas mujeres (6min)
+@app.route('/personas/sexo/<string:sexo>', methods=['GET'])
+def personas_sexo(sexo):
+    conexion = conexionMYSQL.connection.cursor()
+    sentencia = f"select * from t_persona where per_sexo ='{sexo}';"
+    conexion.execute(sentencia)
+    informacion = conexion.fetchall()
+    resultadofinal = []
+    for persona in informacion:
+        diccionario = {
+            'id':persona[0],
+            'nombre':persona[1],
+            'apellidos': persona[2]+' '+persona[3]
+        }
+        resultadofinal.append(diccionario)
+    return jsonify({
+        'mensaje':f'Las personas con el sexo {sexo} son',
+        'contenido':resultadofinal
+    })
+# Ruta para Traer la lista de todos los productos de una categoria enviada por el url (JOIN) (6min)
+@app.route('/categoria/<string:categoria>/productos', methods=['GET'])
+def categoria_productos(categoria):
+    pass
+# Ruta para Ingresar una relacion de una persona con el producto y su cantidad (5min)
+@app.route('/persona/producto', methods=['POST'])
+def persona_producto():
+    pass
 
 if __name__ =="__main__":
     # significa que se va a ejecutar siempre y cuando estemos en nuestra ventana principal del proyecto
